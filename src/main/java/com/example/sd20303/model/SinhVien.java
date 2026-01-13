92820d448169f14359ad97f4a47acbe817e80da0
tuannd05 <nguyendangtuan150705@gmail.com>
2026-01-13 19:31:56 +0700

    up

---
+++ src/main/java/com/example/sd20303/model/SinhVien.java
@@ -0,0 +1,14 @@
+package com.example.sd20303.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SinhVien {
+    private String maSV;
+    private String tenSV;
+    private String khoa;
+}
