c1622fab9c30aed3e3157cb3194c33cba1a00c45
tuannd05 <nguyendangtuan150705@gmail.com>
2026-01-14 09:57:35 +0700

    set

---
+++ src/main/java/com/example/sd20303/model/NhanVien.java
@@ -0,0 +1,14 @@
+package com.example.sd20303.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NhanVien {
+    private  String nhanvien;
+    private String chucvu;
+    private String phongban;
+}
